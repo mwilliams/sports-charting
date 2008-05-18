a003c4ba613e690de9d6a9010e367538677e33f0
Matthew Williams <mwilliams@Danzig.(none)>
2008-05-18 13:39:44 -0400

    Quick modification to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => 'sports_charting', :action => 'index'
+  map.root :controller => 'sports_charting'
 
   # See how all your routes lay out with "rake routes"
 
